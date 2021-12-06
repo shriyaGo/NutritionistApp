bc8e0399616f79de33115870c5eab85f5973cb28
shriyaGlo <shriya.goyal@globallogic.com>
2021-12-06 12:59:06 +0530

    server commit

---
+++ eureka-server/src/test/java/com/globallogic/server/EurekaServerApplicationTest.java
@@ -0,0 +1,13 @@
+package com.globallogic.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
